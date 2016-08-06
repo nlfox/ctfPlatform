08d0fc7985b7e6f8e06a87b35d70b207f6f90b0a
nlfox <nlfox@msn.cn>
2016-08-05 22:56:39 -0400

    intro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,6 @@
 <!--<link href="{{ asset('/css/app.css') }}" rel="stylesheet">-->
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
     <!-- Fonts -->
-    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -17,7 +16,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script src="http://cdn.bootcss.com/jquery/2.1.3/jquery.min.js"></script>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <base href="{{url('/')}}"/>
 </head>
 <body>
@@ -80,7 +79,7 @@
 @yield('content')
 
 <!-- Scripts -->
-<script src="http://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
